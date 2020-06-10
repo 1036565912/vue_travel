0da05fb02e9088f95983c45d76a424c0e48aeab0
chenlin <chenlin@ai-lawyer.net.cn>
2020-06-10 17:05:54 +0800

    完成首页轮播图组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import FastClick from 'fastclick'
+import VueAweSomeSwiper from 'vue-awesome-swiper'
 
 //  引入初始化css样式文件
 //  由于不同设备浏览器的初始样式不一致 这里引入统一的初始化样式
@@ -13,9 +14,15 @@ import 'styles/border.css'
 //  引入iconfont图标样式
 import 'styles/iconfont.css'
 
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 //  由于移动的点击事件存在300毫秒的延迟 导致用户体验不好 这里引入第三方的fastclick包 来解决这个问题
 FastClick.attach(document.body)
+
+//  如果需要配置相关配置信息  可以加第二个参数 options {}
+//  Vue.use(VueAweSomeSwiper, {default settings options})
+Vue.use(VueAweSomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item in slideList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination', // 设置轮播图中的小圆点
+        loop: true, // 设置可以循环跳转
+        autoplay: 3000 // 设置自动播放
+      },
+      slideList: [
+        {
+          id: 1,
+          imgUrl: 'https://www.chenlin.site/jpg/banner_1.jpg'
+        },
+        {
+          id: 2,
+          imgUrl: 'https://www.chenlin.site/jpg/banner_2.jpg'
+        },
+        {
+          id: 3,
+          imgUrl: 'https://www.chenlin.site/jpg/banner_3.jpg'
+        },
+        {
+          id: 4,
+          imgUrl: 'https://www.chenlin.site/jpg/banner_4.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang= "stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff;
+  .wrapper
+    width: 100%;
+    height: 0;
+    overflow: hidden;
+    background-color: #eee;
+    padding-bottom: 40%;
+    .swiper-img
+      width: 100%;
+      height: 3rem;
+</style>
